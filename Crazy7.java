31b6cd2cb12b7e1e433efd2841c4a8f25918929c
clrichardson04 <clrichardson@tamu.edu>
2024-10-19 11:04:53 -0500

    Update Crazy7.java

--- Crazy7.java
+++ Crazy7.java
@@ -1,7 +1,19 @@
 public class Crazy7 {
 public static void main(String[] args){
   ArrayList<String> cardDeck = new ArrayList<String>;
-  
+  System.out.println("Crazy 7s!");
+  System.out.println("Objective: Get Rid of all your cards");
+  System.out.println("Rules:");
+  System.out.println("Two Players (you and the computer)");
+  System.out.println("Each player is delt seven cards");
+  System.out.println("To start the game a card is flipped from the deck");
+  System.out.println("If the card is greater than seven plays a card greater than seven");
+  System.out.println("If the card is less than seven plays a card less than seven");
+  System.out.println("A player can play a seven at any time");
+  System.out.println("If you play the same card that was flipped over you play any other card of your choosing");
+  System.out.println("If a player can't play a necessary card the player draws one from the deck");
+  System.out.println("If niether player can play then a new card is flipped from the deck");
+  System.out.println("If an Ace or King is played then a new card is flipped over from the deck");
 
 
   
