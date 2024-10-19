58ed6e8abd81efd2c5154150b31ccca3c4a5330b
clrichardson04 <clrichardson@tamu.edu>
2024-10-19 11:13:39 -0500

    Update Crazy7.java

--- Crazy7.java
+++ Crazy7.java
@@ -5,19 +5,19 @@ public static void main(String[] args){
 
 
   // Intro Statements
-  System.out.println("Crazy 7s!");
+  System.out.println("\t\tCrazy 7s!");
   System.out.println("Objective: Get Rid of all your cards");
   System.out.println("Rules:");
-  System.out.println("Two Players (you and the computer)");
-  System.out.println("Each player is delt seven cards");
-  System.out.println("To start the game a card is flipped from the deck");
-  System.out.println("If the card is greater than seven plays a card greater than seven");
-  System.out.println("If the card is less than seven plays a card less than seven");
-  System.out.println("A player can play a seven at any time");
-  System.out.println("If you play the same card that was flipped over you play any other card of your choosing");
-  System.out.println("If a player can't play a necessary card the player draws one from the deck");
-  System.out.println("If niether player can play then a new card is flipped from the deck");
-  System.out.println("If an Ace or King is played then a new card is flipped over from the deck \n");
+  System.out.println("\tTwo Players (you and the computer)");
+  System.out.println("\tEach player is dealt seven cards");
+  System.out.println("\tTo start the game a card is flipped from the deck");
+  System.out.println("\tIf the card is greater than seven plays a card greater than seven");
+  System.out.println("\tIf the card is less than seven plays a card less than seven");
+  System.out.println("\tA player can play a seven at any time");
+  System.out.println("\tIf you play the same card that was flipped over you play any other card of your choosing");
+  System.out.println("\tIf a player can't play a necessary card the player draws one from the deck");
+  System.out.println("\tIf neither player can play then a new card is flipped from the deck");
+  System.out.println("\tIf an Ace or King is played then a new card is flipped over from the deck \n");
 
 
 
