9dd3353284d17070d1f94d3c5b1643bfeb76ad52
AshleyHealy <ashleyhealy@tamu.edu>
2024-10-19 11:10:44 -0500

    Update Crazy7.java

--- Crazy7.java
+++ Crazy7.java
@@ -1,6 +1,6 @@
 public class Crazy7 {
 public static void main(String[] args){
-  ArrayList<String> cardDeck = new ArrayList<String>;
+  
 
 
   // Intro Statements
@@ -22,7 +22,8 @@ public static void main(String[] args){
 
 
   // Create the Players hands and the Card deck
-
+  
+  ArrayList<String> cardDeck = new ArrayList<String>;
 
 
 
